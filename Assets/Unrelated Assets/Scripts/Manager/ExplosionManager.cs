1a8c4d8059fa98a7b68283ace64917ed3a45fdf2
d0x <christian.frank.schneider@gmail.com>
2024-02-29 23:56:51 +0100

    Exclude spawning of Explosions from OnDestory

---
+++ Assets/Unrelated Assets/Scripts/Manager/ExplosionManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionManager : MonoBehaviour {
+    public static ExplosionManager INSTANCE;
+
+    private HashSet<Vector3> explosions = new();
+
+    private void Awake() {
+        INSTANCE = this;
+    }
+
+    void Update() {
+        var explosionsCopy = new HashSet<Vector3>(explosions);
+        explosions.Clear();
+
+        foreach (var explosion in explosionsCopy) {
+            spawnExplosion(explosion);
+        }
+    }
+
+    public void addExplosion(Vector3 t) {
+        explosions.Add(t);
+    }
+
+    void spawnExplosion(Vector3 t) {
+        var explosion = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        Destroy(explosion.GetComponent<SphereCollider>());
+
+        explosion.transform.position = t;
+        explosion.transform.localScale = new Vector3(2, 2, 2);
+
+        var explosionController = explosion.AddComponent<ExplosionController>();
+        explosionController.startColor = Color.red;
+        explosionController.endColor = Color.yellow;
+        explosionController.transitionRenderer = explosion.GetComponent<Renderer>();
+
+        explosionController.StartCoroutine(explosionController.ScaleAndDestroy(explosion));
+    }
+}
+
+public class ExplosionController : MonoBehaviour {
+    public Color startColor;
+    public Color endColor;
+    public Renderer transitionRenderer;
+
+    public IEnumerator ScaleAndDestroy(GameObject obj) {
+        float timeElapsed = 0;
+
+        while (timeElapsed < 0.5f) {
+            var targetScale = Mathf.Lerp(2, 5, timeElapsed / 0.2f);
+            obj.transform.localScale = new Vector3(targetScale, targetScale, targetScale);
+            Color newColor = Color.Lerp(startColor, endColor, timeElapsed / 0.5f);
+            transitionRenderer.material.color = newColor;
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(obj);
+    }
+}
--- Assets/_Projectils/ExplosiveForceEmitter.cs
+++ Assets/_Projectils/ExplosiveForceEmitter.cs
@@ -16,7 +16,7 @@ public class ExplosiveForceEmitter : MonoBehaviour {
         }
 
         if (spawnExplosion) {
-            SpawnExplosion();
+            ExplosionManager.INSTANCE.addExplosion(transform.position);
         }
     }
 
@@ -29,41 +29,5 @@ public class ExplosiveForceEmitter : MonoBehaviour {
                     ForceMode.Impulse);
             }
         }
-    }
-
-    private void SpawnExplosion() {
-        var explosion = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        Destroy(explosion.GetComponent<SphereCollider>());
-
-        explosion.transform.position = transform.position;
-        explosion.transform.localScale = new Vector3(2, 2, 2);
-
-        var explosionController = explosion.AddComponent<ExplosionController>();
-        explosionController.startColor = Color.red;
-        explosionController.endColor = Color.yellow;
-        explosionController.transitionRenderer = explosion.GetComponent<Renderer>();
-
-        explosionController.StartCoroutine(explosionController.ScaleAndDestroy(explosion));
-    }
-}
-
-public class ExplosionController : MonoBehaviour {
-    public Color startColor;
-    public Color endColor;
-    public Renderer transitionRenderer;
-
-    public IEnumerator ScaleAndDestroy(GameObject obj) {
-        float timeElapsed = 0;
-
-        while (timeElapsed < 0.5f) {
-            var targetScale = Mathf.Lerp(2, 5, timeElapsed / 0.2f);
-            obj.transform.localScale = new Vector3(targetScale, targetScale, targetScale);
-            Color newColor = Color.Lerp(startColor, endColor, timeElapsed / 0.5f);
-            transitionRenderer.material.color = newColor;
-            timeElapsed += Time.deltaTime;
-            yield return null;
-        }
-
-        Destroy(obj);
     }
 }
