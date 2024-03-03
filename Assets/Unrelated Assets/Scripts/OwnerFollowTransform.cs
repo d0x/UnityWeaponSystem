cfbb5dc5868b65d8983f1ddddb18219c7bb63eb4
d0x <christian.frank.schneider@gmail.com>
2024-03-04 00:05:23 +0100

    Add OwnerFollowTransform script

---
+++ Assets/Unrelated Assets/Scripts/OwnerFollowTransform.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+public class OwnerFollowTransform : NetworkBehaviour {
+    public Transform followTarget;
+
+    public override void OnNetworkSpawn() {
+        if (!IsOwner)
+            enabled = false;
+    }
+
+    private void Update() {
+        if (followTarget != null) {
+            transform.position = followTarget.position;
+            transform.rotation = followTarget.rotation;
+        }
+    }
+}
